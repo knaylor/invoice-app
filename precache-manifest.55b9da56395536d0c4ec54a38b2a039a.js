b812fa6876061c3caee10cd8d0b322406e8c4086
KEVIN NAYLOR <kevin@KLNMBP13.fios-router.home>
2020-01-11 16:03:58 -0500

    Updates

---
+++ precache-manifest.55b9da56395536d0c4ec54a38b2a039a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd66faa76ddf4dcab36ae31c3ec773a7",
+    "url": "/invoice-app/index.html"
+  },
+  {
+    "revision": "50efd4ecbed2789a6b12",
+    "url": "/invoice-app/static/css/main.d259758d.chunk.css"
+  },
+  {
+    "revision": "48a7355ebf16d249d4e7",
+    "url": "/invoice-app/static/js/2.541956c4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/invoice-app/static/js/2.541956c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "50efd4ecbed2789a6b12",
+    "url": "/invoice-app/static/js/main.657b3867.chunk.js"
+  },
+  {
+    "revision": "7a707587ad79d6b14636",
+    "url": "/invoice-app/static/js/runtime-main.c463e7b6.js"
+  }
+]);
